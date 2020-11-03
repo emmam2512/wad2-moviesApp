4f8ee2263adbc3b4024effb06d1f3884b6039682
Emma Murphy <20085230@mail.wit.ie>
2020-11-03 12:05:45 +0000

    Added Upcoming movies page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import FavoritesMoviesPage from './pages/favoritesMoviesPage'       // NEW
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import MovieReviewPage from "./pages/movieReviewPage";
+import UpcomingMoviePage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <SiteHeader />      {/* New Header  */}
           <div className="container-fluid">
             <Switch>
+            <Route exact path="/movies/upcoming" component={UpcomingMoviePage} />
         <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoritesMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
