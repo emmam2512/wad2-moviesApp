5c08745c70a4b6938293f805e470b4bc196b6068
Emma Murphy <20085230@mail.wit.ie>
2020-11-10 11:56:15 +0000

    Add review supported

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import UpcomingMoviePage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader'
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
              <MoviesContextProvider> 
              <GenresContextProvider>
                 <Switch>
+                     <Route exact path="/reviews/form" component={AddMovieReviewPage} />
                      <Route exact path="/movies/upcoming" component={UpcomingMoviePage} />
                      <Route path="/reviews/:id" component={MovieReviewPage} />
                      <Route exact path="/movies/favorites" component={FavoritesMoviesPage} />
