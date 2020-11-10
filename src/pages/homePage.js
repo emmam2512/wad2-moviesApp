b280582717a5fca5d3aa7cc57e5d2745fb46659a
Emma Murphy <20085230@mail.wit.ie>
2020-11-10 11:43:25 +0000

    Refactor add favorites feature

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,15 +5,19 @@ import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
+  const movies = context.movies.filter((m) => {
+
+    return !("favorite" in m);
+  });
 
   return (
-      <PageTemplate 
-        title='No. Movies'
-        movies={context.movies}
-        action={(movie) => {
-          return <AddToFavoritesButton movie={movie} /> 
-        }}
-      />
+    <PageTemplate
+      title="No. Movies"
+      movies={movies}  /* Changed */
+      action={(movie) => {
+        return <AddToFavoritesButton movie={movie} />;
+      }}
+    />
   );
 };
 
